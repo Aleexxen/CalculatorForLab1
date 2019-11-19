4a2eb3f625fcdca260e8a99d817dd80f43254890
Aleexxen <Alekseewa.xenya@yandex.ru>
2019-11-19 18:47:28 +0300

    First Commit

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,31 @@
+package src.com.company;
+
+public class Calculator {
+    public Drob sum(Drob first, Drob second) {
+        int numerator = first.getNumerator() * second.getDenominator() + second.getNumerator() * first.getDenominator();
+        int denominator = first.getDenominator() * second.getDenominator();
+
+        return new Drob(numerator, denominator);
+    }
+
+    public Drob minus(Drob first, Drob second) {
+        int numerator = first.getNumerator() * second.getDenominator() - second.getNumerator() * first.getDenominator();
+        int denominator = first.getDenominator() * second.getDenominator();
+
+        return new Drob(numerator, denominator);
+    }
+
+    public Drob times(Drob first, Drob second) {
+        int numerator = first.getNumerator() * second.getNumerator();
+        int denominator = first.getDenominator() * second.getDenominator();
+
+        return new Drob(numerator, denominator);
+    }
+
+    public Drob divide(Drob first, Drob second) {
+        int numerator = first.getNumerator() * second.getDenominator();
+        int denominator= first.getDenominator() * second.getNumerator();
+
+        return new Drob(numerator, denominator);
+    }
+}
---
+++ src/com/company/Market.java
@@ -0,0 +1,61 @@
+package src.com.company;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Market {
+
+    public static void main(String[] args) throws IOException {
+        List<Integer> input = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("./src/com/company/input.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                input.add(Integer.parseInt(line));
+            }
+        }
+
+        List<Drob> drobs = new ArrayList<>();
+        for (int i = 0; i < input.size() - 1; i += 2) {
+            drobs.add(new Drob(input.get(i), input.get(i + 1)));
+        }
+
+        Scanner in = new Scanner(System.in);
+
+        // Index of first Drob starting from 0.
+        System.out.print("Input a number: ");
+        int left = in.nextInt();
+        // Operation sign
+        System.out.print("Input an operation: ");
+        String op = in.next();
+        // Index of second Drob starting from 0.
+        System.out.print("Input a number: ");
+        int right = in.nextInt();
+        in.close();
+
+        System.out.println("Drob[" + left + "] " + op + " Drob[" + right + "]");
+        Calculator calculator = new Calculator();
+        Drob result = null;
+
+        if (op.equals("+")) {
+            result = calculator.sum(drobs.get(left), drobs.get(right));
+        } else if (op.equals("-")) {
+            result = calculator.minus(drobs.get(left), drobs.get(right));
+        } else if (op.equals("*")) {
+            result = calculator.times(drobs.get(left), drobs.get(right));
+        } else if (op.equals("/")) {
+            result = calculator.divide(drobs.get(left), drobs.get(right));
+        } else {
+            System.out.println("Only `+`, `-`, `*`, `/` operations are supported");
+        }
+
+        if (result != null) {
+            System.out.println(result);
+        }
+
+    }
+
+}
